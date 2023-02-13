c72c1143296d0d0789abea25b92c80b70572eec2
pyteres <ianpimenta18@gmail.com>
2023-02-13 06:09:03 -0300

    por-que-usar

---
+++ por-que-usar/src/orientaçaoAObjeto/orientaçaoAObjeto.java
@@ -0,0 +1,21 @@
+package orientaçaoAObjeto;
+
+public class orientaçaoAObjeto {
+  //Explicar por que devemos progamar em orientaçao a objeto.
+  /*PEE
+   * Paradgma estruturado: tem uma estrutura mais simplista.
+   * Paradgmas estruturados: foca em operaçoes funçoes e dados.
+   * Paradgma estruturado: Foca mais como fazer.
+   */
+
+   /*POO
+    * Paradgma orientado a objeto: tem uma estrutura mais simplista.
+    * Paradgma orientado a objeto: Foca na modelagem de entidades e na interaçao entre eles.
+    * Paradgma oreintado a objeto: foca mais em o que fazer
+    //Vantagens:
+    //melhor coesao.
+    //melhor acoplamento.
+    //Diminuiçao do gap semantico.
+    //Coletor de lixo.
+    */
+}
