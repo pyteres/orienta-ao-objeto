31e3da372a3fe64e6c20990e5e308134defae808
pyteres <ianpimenta18@gmail.com>
2023-02-13 06:40:33 -0300

    fundamentos

---
+++ fundamentos/src/orientaçaoAObjeto/fundamento.java
@@ -0,0 +1,21 @@
+package orientaçaoAObjeto;
+
+public class fundamento {
+/*Definiçao
+ * A oreintaçao a objeto e  um paradgma de analise, projetos e progamçao de sitema de software  baseado na  composiçao e interaçao entre diversas unidades de software chamados de objetos
+ */
+//Fundamentos:
+/*Abstraçao
+ * Processo pelo qual se isola caracteresticas de um objeto considerando os que tenham em comum certo grupos de objetos.
+ * Reuso:
+ * Capacidade de criar novas unidades de codigo  a partir de outras ja existentes.
+ * Emcapsulamento:
+ * Capacidade  de esconder complexadade e protager dados.
+ */
+ //Exemplo:
+  //livro:
+   //nome:
+   //autor:
+   //editora:
+   //numero de paginas:
+}
